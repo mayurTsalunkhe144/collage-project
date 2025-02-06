4d2c0296c233c0af713f9e4eb65e09fbc55cf212
Mayur <mayurtsalunkhe@gmail.com>
2025-02-06 13:35:28 +0530

    first version ui update

--- app/(root)/home/page.tsx
+++ app/(root)/home/page.tsx
@@ -3,11 +3,11 @@
 
 
 import { useUser } from '@clerk/nextjs'
-import { currentUser } from '@clerk/nextjs/server'
+
 import React from 'react'
 
 const Home = () => {
-  const {user}=useUser();
+
   return (
     <div>
 
